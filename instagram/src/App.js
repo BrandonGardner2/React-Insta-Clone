fd805834f1b5a1a3e498e6d39b31f9510a4a4e10
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-18 11:39:24 -0600

    Doing some organizing

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>Instagram</h2>
-        <p>Wow</p>
-      </div>
-    );
-  }
-}
+import dummyData from "./dummy-data";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Instagram</h2>
+      <p>Wow</p>
+    </div>
+  );
+};
 
 export default App;
