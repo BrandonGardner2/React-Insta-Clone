e2e08b939e444ad2bf6df99216be607879543fd4
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-21 14:56:16 -0600

    Search field inner styled component

--- instagram/src/Components/SearchBar/SearchField.js
+++ instagram/src/Components/SearchBar/SearchField.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchFieldComponent = styled.form`
+@media screen and (max-width: 550px) {
+    display: none;
+`;
+
+const SearchFieldInput = styled.input`
+  text-align: center;
+  &:focus {
+    outline: none;
+  }
+`;
 
 const SearchField = props => {
   const [text, setText] = useState("");
   return (
-    <form
+    <SearchFieldComponent
       className="search-field"
       onSubmit={e => {
         props.filterPosts(e, text);
         setText("");
       }}
     >
-      <input
+      <SearchFieldInput
         type="text"
         placeholder="Search"
         onChange={e => setText(e.target.value)}
         value={text}
       />
-    </form>
+    </SearchFieldComponent>
   );
 };
 
